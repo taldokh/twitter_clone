90ba73fc2ccb202e9b300dbe4a766c5b92782c00
taldokh <taldokh@gmail.com>
2021-04-23 19:07:44 +0300

    added and positiond user's avatar

---
+++ FlutterFrontend/lib/screens/user_profile/user_profile.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../../data.dart';
+import '../../widgets/user_post.dart';
+import '../../models/fetched_post.dart';
+import '../../models/user.dart';
+
+class UserProfile extends StatelessWidget {
+  User _user;
+  UserProfile(int userID) {
+    this._user = _fetchUser(userID);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Profile'),
+      ),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: this._user.imagePath.image,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  User _fetchUser(int userID) {
+    return users.singleWhere((user) => user.id == userID);
+  }
+}
