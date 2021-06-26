60b4411195966872e6c31a792b71479e3718f241
taldokh <taldokh@gmail.com>
2021-06-26 20:36:06 +0300

    add navigation to profile pages by taping the avatar

--- FlutterFrontend/lib/models/fetched_post.dart
+++ FlutterFrontend/lib/models/fetched_post.dart
@@ -8,9 +8,10 @@ class FetchedPost {
   final String _handle;
   final String _uploadTime;
   final Content _content;
+  final int _userID;
 
   FetchedPost(this._userImage, this._name, this._handle, this._uploadTime,
-      this._content);
+      this._content, this._userID);
 
   Image get userImage => this._userImage;
 
@@ -21,4 +22,6 @@ class FetchedPost {
   String get uploadTime => this._uploadTime;
 
   Content get content => this._content;
+
+  int get userID => this._userID;
 }
