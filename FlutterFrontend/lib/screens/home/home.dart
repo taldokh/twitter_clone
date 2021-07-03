93adab82ef504c36f76837db6159582ea6678c28
taldokh <taldokh@gmail.com>
2021-07-03 19:41:00 +0300

    create drawer with navigation to profile page

--- FlutterFrontend/lib/screens/home/home.dart
+++ FlutterFrontend/lib/screens/home/home.dart
@@ -1,3 +1,7 @@
+import 'package:FlutterFrontend/navigation/routes.dart';
+import 'package:FlutterFrontend/screens/user_profile/follow_info.dart';
+import 'package:FlutterFrontend/widgets/handle.dart';
+import 'package:FlutterFrontend/widgets/user_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../data.dart';
@@ -7,10 +11,57 @@ import './../../widgets/twitter_app_bar.dart';
 import './../../models/session_state.dart';
 
 class Home extends StatelessWidget {
+  static const int _HamburgerIconColor = 0xFF1DA1F2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: Colors.white, title: TwitterAppBar()),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                  child: Container(
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Consumer<SessionState>(
+                        builder: (context, session, child) {
+                      return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            UserAvatar(
+                              _userImageById(session.userID).image,
+                              avatarCircleRadius: 30,
+                            ),
+                            Text(
+                              _userNameById(session.userID),
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Handle(_userHandleById(session.userID)),
+                            FollowInfo(_userFollowingById(session.userID),
+                                _userFollowersById(session.userID))
+                          ]);
+                    })),
+              )),
+              Consumer<SessionState>(builder: (context, session, child) {
+                return ListTile(
+                  leading: Icon(Icons.account_circle_outlined),
+                  title: Text('Profile'),
+                  onTap: () {
+                    Navigator.pushNamed(context, ProfileRoute,
+                        arguments: {'userID': session.userID});
+                  },
+                );
+              }),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: TwitterAppBar(),
+          iconTheme: IconThemeData(color: Color(_HamburgerIconColor)),
+        ),
         body: Consumer<SessionState>(
           builder: (context, session, child) {
             return ListView(children: [..._fetchHomeWallPosts(session.userID)]);
@@ -53,4 +104,12 @@ String _userHandleById(int userId) {
 
 String _userNameById(int userId) {
   return users.firstWhere((user) => user.id == userId).name;
+}
+
+String _userFollowersById(int userId) {
+  return users.firstWhere((user) => user.id == userId).followersCount;
+}
+
+String _userFollowingById(int userId) {
+  return users.firstWhere((user) => user.id == userId).followingCount;
 }
