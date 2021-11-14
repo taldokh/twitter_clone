5dec787f716a50a05cf731a8a05daae10e26d80c
taldokh <taldokh@gmail.com>
2021-11-14 07:48:21 +0200

    testing workflow

--- NodeJSBackend/app.ts
+++ NodeJSBackend/app.ts
@@ -13,8 +13,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_req: any, res: any) => {
-    //res.send('hello world');
-    res.send('Welcome');
+    res.send('hello world');
 });
 
 app.use('/post', postController);
